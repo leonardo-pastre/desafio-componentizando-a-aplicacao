ea6d550463790c011748f75f26507b5ad19ad953
work\leonardo-pastre <tp.l.poliveira@d0007azurecloudtotvscom.onmicrosoft.com>
2023-04-15 00:32:22 -0300

    Resolução do desafio, criando novos componentes e separando algumas responsabilidades

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,54 @@
-export function Content() {
-  // Complete aqui
+import { useState, useEffect } from 'react';
+
+import { MovieCard } from './MovieCard';
+import { ContentHeader } from './ContentHeader';
+
+import { api } from '../services/api';
+
+import '../styles/content.scss';
+
+interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+interface ContentProps {
+  selectedGenreId: number;
+  selectedGenre: GenreResponseProps;
+}
+
+export function Content(props: ContentProps) {
+  const [movies, setMovies] = useState<MovieProps[]>([]);
+
+  useEffect(() => {
+    api.get<MovieProps[]>(`movies/?Genre_id=${props.selectedGenreId}`).then(response => {
+      setMovies(response.data);
+    });
+  }, [props.selectedGenreId]);
+
+  return(
+    <div className="container">
+      <ContentHeader title={props.selectedGenre.title}/>
+
+      <main>
+        <div className="movies-list">
+          {movies.map(movie => (
+            <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+          ))}
+        </div>
+      </main>
+    </div>
+  )
 }
